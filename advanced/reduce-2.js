f23cc8d8ed39639cdbb26adfbfb8957c057faa86
zahid4523114 <dewanmohammodzahidhasan@gmail.com>
2022-08-25 22:17:35 -0600

    add all

--- advanced/reduce-2.js
+++ advanced/reduce-2.js
@@ -1,9 +1,11 @@
+//Challenging Follow above array of objects. So, you have 3 objects as array element. Can you find out the total sum from here?
+
 let peoples = [
   { name: "meena", age: 20 },
   { name: "rina", age: 15 },
   { name: "tina", age: 22 },
 ];
-//calculation with for loop.
+//Yeah! Do it with for loop.
 let sum = 0;
 for (let i = 0; peoples.length > i; i++) {
   let getAge = peoples[i].age;
@@ -11,10 +13,16 @@ for (let i = 0; peoples.length > i; i++) {
 }
 console.log(sum);
 
-//calculation with reduce method
+//Do the same task using array.reduce() method.
 
 let peoples2 = [
   { name: "meena", age: 20 },
   { name: "rina", age: 15 },
   { name: "tina", age: 22 },
 ];
+
+let checkAge = peoples2.reduce((pValue, cValue) => {
+  return pValue + cValue.age;
+}, 0);
+
+console.log(checkAge);
---
+++ practice problem/four.js
@@ -0,0 +1,33 @@
+//Difference between Function Declaration and Function Expression:
+
+//Function Declaration
+//1.A function declaration must have a function name.
+
+//2.Function declaration does not require a variable assignment.
+
+//3.These are executed before any other code.
+
+//4.The function in function declaration can be accessed before and after the function definition.
+
+//5.Function declarations are hoisted.
+
+//6.Syntax:
+//function geeksforGeeks(paramA, paramB) {
+// Set of statements
+//}
+
+//Function Expression
+//1.A function Expression is similar to a function declaration without the function name.
+
+//2.Function expressions can be stored in a variable assignment.
+
+//3.Function expressions load and execute only when the program interpreter reaches the line of code.
+
+//4.The function in function declaration can be accessed only after the function definition.
+
+//5.Function expressions are not hoisted.
+
+//6.Syntax:
+//var geeksforGeeks= function(paramA, paramB) {
+// Set of statements
+//}
